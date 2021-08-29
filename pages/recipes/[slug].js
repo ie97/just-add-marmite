0bec159d761775398caefb611a0293f240a193a3
ie97 <iwan.ie.id@gmail.com>
2021-08-29 16:14:50 +0700

    fallback

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from "contentful";
 import Image from 'next/image';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import Skeleton from "../../components/Skeleton";
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -21,7 +22,8 @@ export const getStaticPaths = async() => {
   return{
     // return array of path
     paths: paths, 
-    fallback: false /* 404 page */
+    //fallback: false // 404 page
+    fallback: true //fallback page
   }
 }
 
@@ -38,6 +40,8 @@ export async function getStaticProps({ params }) {
 }
 
 export default function RecipeDetails({ recipe }) {
+  if(!recipe) return <Skeleton />
+  
   const {featuredImage, title, cookingTime, ingredients, method} = recipe.fields
   console.log(featuredImage)
   return (
